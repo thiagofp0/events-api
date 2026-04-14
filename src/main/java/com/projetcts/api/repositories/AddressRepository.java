5679887c4658e32ff4daa5b6f916004b4cf50652
Thiago Ferreira <thiagofp707@gmail.com>
2026-04-13 23:05:56 -0300

    Creating repositories

---
+++ src/main/java/com/projetcts/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.projetcts.api.repositories;
+
+import com.projetcts.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
---
+++ src/main/java/com/projetcts/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.projetcts.api.repositories;
+
+import com.projetcts.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
